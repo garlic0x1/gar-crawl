87e45d952d89d4d1ba5763dfd661f4cdc96d6210
garlic0x1 <garlic1@protonmail.com>
2022-08-21 17:53:09 -0700

    broken test?

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,23 @@ mod tests {
     #[tokio::test]
     async fn it_works() {
         let mut seen: HashSet<String> = HashSet::new();
+
         Crawler::builder()
-            .add_default_propagators()
-            .whitelist("l")
-            .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36".into())
-            .add_handler("*[href]", move |el: ElementRef, url: Url| {
-                println!("{:?}", el.value().attr("href"));
-                seen.insert(url.to_string());
-            })
-            .depth(1)
-            .build()
-            .unwrap()
-            .crawl("https://www.vim.org/about.php")
-            .await
-            .unwrap();
+        .add_default_propagators()
+        .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36".into())
+        .add_handler("*[href]", move |el: ElementRef, url: Url| {
+            if let Some(href) = el.value().attr("href") {
+                if let Ok(abs_url) = url.join(href) {
+                    seen.insert(abs_url.to_string());
+                } else {
+                    seen.insert(href.to_string());
+                }
+            }
+        })
+        .depth(1)
+        .build().unwrap()
+        .crawl("https://vim.org/weird.php")
+        .await.unwrap();
 
         assert_eq!(seen.len(), 32);
     }
