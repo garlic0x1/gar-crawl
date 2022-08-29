30c12f96fd35e626a8f20af59e5c23bd8f0061c1
garlic0x1 <garlic1@protonmail.com>
2022-08-28 17:45:18 -0700

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,25 @@ pub use auxiliary::absolute_url;
 
 #[cfg(test)]
 mod tests {
+    use reqwest::Url;
+
+    use crate::absolute_url;
 
     #[tokio::test]
-    async fn crawl_test() {}
+    async fn abs_url() {
+        let base_url = Url::parse("https://github.com/garlic0x1/").unwrap();
+        let href = "gar-crawl/";
+        let abs_url = absolute_url(&base_url, href).unwrap();
+        assert_eq!(abs_url.as_str(), "https://github.com/garlic0x1/gar-crawl/");
+
+        let base_url = Url::parse("https://github.com/garlic0x1").unwrap();
+        let href = "gar-crawl/";
+        let abs_url = absolute_url(&base_url, href).unwrap();
+        assert_eq!(abs_url.as_str(), "https://github.com/gar-crawl/");
+
+        let base_url = Url::parse("https://github.com/garlic0x1/").unwrap();
+        let href = "https://google.com";
+        let abs_url = absolute_url(&base_url, href).unwrap();
+        assert_eq!(abs_url.as_str(), "https://google.com/");
+    }
 }
